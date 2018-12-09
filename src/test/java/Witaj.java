8deb873aa1f406c71ecdd08f0637def6277f0c4d
ExceliAdam <adam@exceliadam.pl>
2018-12-09 13:21:49 +0100

    Obsługa exit

--- src/test/java/Witaj.java
+++ src/test/java/Witaj.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Witaj {
 
         public static void main(String[] args)
         {
             System.out.println("Witaj!");
+
+            Scanner skaner = new Scanner(System.in);
+            System.out.println("Żeby wyłączyć program napisz 'exit'");
+            String tekst;
+
+            do
+            {
+                tekst = skaner.nextLine();
+                if (!tekst.equals("exit")){
+                    System.out.println("Wpisałeś '" + tekst + "' żeby wyjść z aplikacji musisz wpisać 'exit'.");
+                }
+            }while (!tekst.equals("exit"));
+
         }
 
 }
